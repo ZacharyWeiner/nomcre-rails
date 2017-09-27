b073de3108bd0c31bb23a7e614c54071f16f8224
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-09-27 13:43:26 -0300

    redirect to adminlte base page on successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
+    unless current_user.nil?
+      redirect_to adminlte_path
+    end
   end
 end
