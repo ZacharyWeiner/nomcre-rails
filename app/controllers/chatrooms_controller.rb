4d10a9b15f3eb4f6be2d9df67835c3ebb1084280
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-10-01 14:37:44 -0300

    removed label for count if count == 0

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,5 +3,10 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    notifications = Notification.where(user: current_user, notification_type: "New Message", notification_obeject_id: params[:id], read:false)
+    notifications.each do |note|
+      note.read = true
+      note.save
+    end
   end
 end
